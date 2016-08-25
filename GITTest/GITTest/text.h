3a16a0697c3d4d262d0dac4308ea87812cf8743e
guoyichang <15153511628@163.com>
2016-08-25 09:18:20 +0800

    增加

---
+++ GITTest/GITTest/text.h
@@ -0,0 +1,13 @@
+//
+//  text.h
+//  GITTest
+//
+//  Created by jredu on 16/8/25.
+//  Copyright © 2016年 jereh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface text : NSObject
+
+@end
---
+++ GITTest/GITTest/text.m
@@ -0,0 +1,13 @@
+//
+//  text.m
+//  GITTest
+//
+//  Created by jredu on 16/8/25.
+//  Copyright © 2016年 jereh. All rights reserved.
+//
+
+#import "text.h"
+
+@implementation text
+
+@end
